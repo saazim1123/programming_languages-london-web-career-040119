7b469d0d31d8cc929ccd0919e12b209732198438
Adnan Azim <saazim1123@gmail.com>
2019-03-23 08:03:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   # your code here
   hash = {}
   languages.each do |style, description|
-    description.each do ||
+    description.each do |l, t|
       
     end
   end
