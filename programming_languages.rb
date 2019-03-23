1a95bb185107186405a648333c2b6b94a4c5946e
Adnan Azim <saazim1123@gmail.com>
2019-03-23 08:01:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+# Iterate over the hash and build a new hash that has the languages as keys that point to a value of a hash that describes the type and style.
+
 def reformat_languages(languages)
   # your code here
+  hash = {}
+  languages.each do |type, description|
+    description.each do |language, type|
+      if hash.has_key?(language)
+        hash[language][:type] << type
+        
+      else
+        hash[language] = type
+        hash[language][:type] = [type]
+      end
+    end
+  end
+  hash
 end
